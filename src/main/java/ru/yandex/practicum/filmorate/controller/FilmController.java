c414683bae761f26951ef1ae3ce29cfdf53d04d1
Savelev_AU <andreisavelv@yandex.ru>
2025-05-03 08:25:42 +0300

    Выполнение 11 ТЗ 2

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -18,7 +18,7 @@ import java.util.List;
 @RequestMapping("/films")
 @Slf4j
 public class FilmController {
-
+// коммент что бы сделать новый пулреквест.
     private final FilmService filmService;
 
     @Autowired
