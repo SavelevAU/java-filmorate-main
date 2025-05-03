6480f4c5c80286e84e8d381c9fc0b33618ff7ba7
Savelev_AU <andreisavelv@yandex.ru>
2025-05-03 10:13:56 +0300

    Выполнение 11 ТЗ исправил ошибку

--- src/test/java/ru/yandex/practicum/filmorate/FilmTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmTests.java
@@ -29,7 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class FilmControllerTest {
+class FilmTests {
 
 	@Autowired
 	private MockMvc mockMvc;
--- src/test/java/ru/yandex/practicum/filmorate/UserTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserTest.java
@@ -26,7 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class UserControllerTest {
+class UserTest {
 
     @Autowired
     private MockMvc mockMvc;
